93157facd4b14401165a2efbbecb6619567b77da
pezzaliapp <pezzalialessandro@gmail.com>
2025-03-04 09:26:20 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
+// Service Worker base per CSVXpress
 
+self.addEventListener('install', event => {
+  console.log('Service Worker installato');
+  // Salta l'attesa per l'installazione
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker attivato');
+});
+
+self.addEventListener('fetch', event => {
+  // Strategia semplice: prova a fare il fetch e, in caso di errore, recupera dal cache (se implementato in futuro)
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
